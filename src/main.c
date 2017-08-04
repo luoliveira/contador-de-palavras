58ccc13c43e73ad7ec777bf7dc3fae22d1f15110
Luana <oliveira.m.luana@gmail.com>
2017-08-04 03:21:44 -0300

    codigo pronto

--- src/main.c
+++ src/main.c
@@ -9,14 +9,25 @@
 
 int main() {
 
-  char c;
+  char c = 0, ant = 0, ant2 = 0;
+  int cont = 0;
 
-  c = 1;
-
-  while (c != '\n') {
+  do {
+    ant2 = ant;
+    ant = c;
     scanf("%c", &c);
-  }
+    /* Conta a palavra a partir do primeiro separador (espaco ou pontuacao) */
+    if (c == ' ' || c == '.' || c == ',' || c == '!' || c == '-' || c == ';') {
+	if (ant != ' ' && ant != '.' && ant != ',' && ant != '!' && ant != '-' && ant != ';') cont++;
+    }
+    /* Trata para que um float nao seja contado 2 vezes */
+    if (c >= '0' && c <= '9' && (ant == '.' || ant == ',') && ant2 >= '0' && ant2 <= '9') cont--;
+	    
+  } while (c != '\n');
 
-  printf("1\n");
+  /* Conta a ultima palavra antes do \n, caso nao tenha sido contada antes */
+  if (ant >= 'A' && ant <= 'z') cont++;
+
+  printf("%d\n", cont);
   return 0;
 }
